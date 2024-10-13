3a88921ff4f6abd11fdd09129ec588b49c4137f3
Le Quoc Uy <lequocuyit@gmail.com>
2024-10-13 15:39:03 +0700

    add

---
+++ LeQuocUy_SE172445_SE1711_Fall24_FE/DTO/SystemAccountDTO.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DTO
+{
+    public partial class SystemAccountDTO
+    {
+        [Key]
+        public short AccountId { get; set; }
+
+        [Required(ErrorMessage = "Account Name is required.")]
+        [StringLength(100, ErrorMessage = "Account Name cannot be longer than 100 characters.")]
+        public string AccountName { get; set; }
+
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address.")]
+        public string AccountEmail { get; set; }
+
+        [Required(ErrorMessage = "Role is required.")]
+        public int AccountRole { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
+        [StringLength(50, MinimumLength = 6, ErrorMessage = "Password must be at least 6 characters long.")]
+        public string AccountPassword { get; set; }
+    }
+}
---
+++ LeQuocUy_SE172445_SE1711_Fall24_FE/DTO/TagDTO.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DTO
+{
+    public partial class TagDTO
+    {
+        public int TagId { get; set; }
+
+        [Required(ErrorMessage = "Tag Name is required.")]
+        [StringLength(100, ErrorMessage = "Tag Name cannot exceed 100 characters.")]
+        public string TagName { get; set; }
+
+        [Required(ErrorMessage = "Tag Note is required.")]
+        [StringLength(200, ErrorMessage = "Note cannot exceed 200 characters.")]
+        public string Note { get; set; }
+
+        public virtual ICollection<NewsArticleResponse>? NewsArticles { get; set; } = new List<NewsArticleResponse>();
+    }
+}
